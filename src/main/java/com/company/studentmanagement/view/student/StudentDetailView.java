ede2583fbc82f71e9ae808d8c929ed37c5447ce9
hieunmhe171547 <hieubeo5d@gmail.com>
2025-02-13 13:34:05 +0700

    Fixed old Bug Student-Class when CRUD (DONE)

--- src/main/java/com/company/studentmanagement/view/student/StudentDetailView.java
+++ src/main/java/com/company/studentmanagement/view/student/StudentDetailView.java
@@ -24,40 +24,40 @@ public class StudentDetailView extends StandardDetailView<Student> {
     @ViewComponent
     private DataContext dataContext;
 
-    @Subscribe("classField")
-    public void onClassFieldComponentValueChange(final AbstractField.ComponentValueChangeEvent<EntityComboBox<Klass>, Klass> event) {
-        var selectedClass = event.getValue();
-        if (currentClass == null || currentClass.getId() == selectedClass.getId()) {
-            return;
-        }
-        Klass oldClass = dataManager.load(Klass.class)
-                .query("SELECT c FROM Klass c WHERE c.id = :classId")
-                .parameter("classId", currentClass.getId())
-                .optional()
-                .orElse(null);
-        Klass newClass = dataManager.load(Klass.class)
-                .query("SELECT c FROM Klass c WHERE c.id = :classId")
-                .parameter("classId", selectedClass.getId())
-                .optional()
-                .orElse(null);
-
-        boolean updated = false;
-
-        if (oldClass != null) {
-            oldClass.setTotalStudent(oldClass.getTotalStudent() - 1);
-            updated = true;
-        }
-
-        if (newClass != null) {
-            newClass.setTotalStudent(newClass.getTotalStudent() + 1);
-            updated = true;
-        }
-
-        if (updated) {
-            dataManager.save(oldClass, newClass);
-        }
-        currentClass = selectedClass;
-    }
+//    @Subscribe("classField")
+//    public void onClassFieldComponentValueChange(final AbstractField.ComponentValueChangeEvent<EntityComboBox<Klass>, Klass> event) {
+//            var selectedClass = event.getValue();
+//        if (currentClass == null || currentClass.getId() == selectedClass.getId()) {
+//            return;
+//        }
+//        Klass oldClass = dataManager.load(Klass.class)
+//                .query("SELECT c FROM Klass c WHERE c.id = :classId")
+//                .parameter("classId", currentClass.getId())
+//                .optional()
+//                .orElse(null);
+//        Klass newClass = dataManager.load(Klass.class)
+//                .query("SELECT c FROM Klass c WHERE c.id = :classId")
+//                .parameter("classId", selectedClass.getId())
+//                .optional()
+//                .orElse(null);
+//
+//        boolean updated = false;
+//
+//        if (oldClass != null) {
+//            oldClass.setTotalStudent(oldClass.getTotalStudent() - 1);
+//            updated = true;
+//        }
+//
+//        if (newClass != null) {
+//            newClass.setTotalStudent(newClass.getTotalStudent() + 1);
+//            updated = true;
+//        }
+//
+//        if (updated) {
+//            dataManager.save(oldClass, newClass);
+//        }
+//        currentClass = selectedClass;
+//    }
 
 
     @Subscribe
