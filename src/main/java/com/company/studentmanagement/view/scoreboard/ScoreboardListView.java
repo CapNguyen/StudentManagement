79e704d503a7a60a068cdcc20c98995f3f628962
hieunmhe171547 <hieubeo5d@gmail.com>
2025-02-13 15:01:20 +0700

    Pagination for ScoreboardListView

--- src/main/java/com/company/studentmanagement/view/scoreboard/ScoreboardListView.java
+++ src/main/java/com/company/studentmanagement/view/scoreboard/ScoreboardListView.java
@@ -2,8 +2,15 @@ package com.company.studentmanagement.view.scoreboard;
 
 import com.company.studentmanagement.entity.Scoreboard;
 import com.company.studentmanagement.view.main.MainView;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Focusable;
 import com.vaadin.flow.router.Route;
+import io.jmix.core.DataManager;
+import io.jmix.flowui.component.grid.DataGrid;
+import io.jmix.flowui.component.grid.editor.DataGridEditor;
+import io.jmix.flowui.model.InstanceContainer;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Route(value = "scoreboards", layout = MainView.class)
@@ -12,6 +19,28 @@ import io.jmix.flowui.view.*;
 @LookupComponent("scoreboardsDataGrid")
 @DialogMode(width = "64em")
 public class ScoreboardListView extends StandardListView<Scoreboard> {
+    @Autowired
+    private DataManager dataManager;
+    @ViewComponent
+    private DataGrid<Scoreboard> scoreboardsDataGrid;
+
+    @Subscribe(id = "scoreboardsDc", target = Target.DATA_CONTAINER)
+    public void onScoreboardsDcItemPropertyChange(final InstanceContainer.ItemPropertyChangeEvent<Scoreboard> event) {
+        dataManager.save(event.getItem());
+    }
+
+    @Subscribe
+    public void onInit(final InitEvent event) {
+        DataGridEditor<Scoreboard> editor = scoreboardsDataGrid.getEditor();
+        scoreboardsDataGrid.addItemDoubleClickListener(
+                s -> {
+                    editor.editItem(s.getItem());
+                    Component editorComponent = s.getColumn().getEditorComponent();
+                    if (editorComponent instanceof Focusable) {
+                        ((Focusable) editorComponent).focus();
+                    }
+                });
+    }
 
 
 }
