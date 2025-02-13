1c5eb952248114142264370b9e8526069a5b207c
hieunmhe171547 <hieubeo5d@gmail.com>
2025-02-13 14:05:17 +0700

    Delete unused view (Scoreboard Detail) Add filter for all view

--- src/main/java/com/company/studentmanagement/view/scoreboard/ScoreboardListView.java
+++ src/main/java/com/company/studentmanagement/view/scoreboard/ScoreboardListView.java
@@ -1,18 +1,9 @@
 package com.company.studentmanagement.view.scoreboard;
 
-import com.company.studentmanagement.entity.Klass;
-import com.company.studentmanagement.entity.Major;
 import com.company.studentmanagement.entity.Scoreboard;
-import com.company.studentmanagement.entity.Student;
 import com.company.studentmanagement.view.main.MainView;
 import com.vaadin.flow.router.Route;
-import io.jmix.core.DataManager;
-import io.jmix.flowui.model.CollectionLoader;
 import io.jmix.flowui.view.*;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
-import java.util.Random;
 
 
 @Route(value = "scoreboards", layout = MainView.class)
@@ -21,42 +12,6 @@ import java.util.Random;
 @LookupComponent("scoreboardsDataGrid")
 @DialogMode(width = "64em")
 public class ScoreboardListView extends StandardListView<Scoreboard> {
-//    private List<Scoreboard> scoreboards;
-//    @Autowired
-//    private DataManager dataManager;
-//    @ViewComponent
-//    private CollectionLoader<Scoreboard> scoreboardsDl;
-//
-//    @Subscribe
-//    public void onBeforeShow(final BeforeShowEvent event) {
-//        List<Klass> classes = dataManager.load(Klass.class).all().list();
-//        for (Klass c : classes) {
-//            bindMajorToStudentInClass(c);
-//        }
-//        scoreboardsDl.load();
-//    }
-//
-//    private void bindMajorToStudentInClass(Klass c) {
-//        List<Student> students = dataManager.load(Student.class)
-//                .query("SELECT s FROM Student s " +
-//                        "Where s.inClass.id = :classId")
-//                .parameter("classId", c.getId())
-//                .list();
-//        List<Major> majors = dataManager.load(Major.class)
-//                .query("SELECT m FROM Major m " +
-//                        "JOIN Lecturer l ON l.major = m " +
-//                        "JOIN LecturerClass lc ON lc.lecturer = l " +
-//                        "WHERE lc.klass.id = :classId")
-//                .parameter("classId", c.getId())
-//                .list();
-//        for (Major m : majors) {
-//            for (Student s : students) {
-//                var sc = dataManager.create(Scoreboard.class);
-//                sc.setMajor(m);
-//                sc.setStudent(s);
-//                dataManager.save(sc);
-//            }
-//        }
-//    }
+
 
 }
