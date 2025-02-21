3c123ca1af8ec7fb8c3ad97458bf1c606c554550
hieunm150 <hieunm150@fpt.com>
2025-02-21 09:40:31 +0700

    Select Lecturer and Student at ClassView

--- src/main/java/com/company/studentmanagement/view/scoreboard/ScoreboardListView.java
+++ src/main/java/com/company/studentmanagement/view/scoreboard/ScoreboardListView.java
@@ -2,15 +2,21 @@ package com.company.studentmanagement.view.scoreboard;
 
 import com.company.studentmanagement.entity.Scoreboard;
 import com.company.studentmanagement.view.main.MainView;
+import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Focusable;
 import com.vaadin.flow.router.Route;
 import io.jmix.core.DataManager;
 import io.jmix.flowui.component.grid.DataGrid;
 import io.jmix.flowui.component.grid.editor.DataGridEditor;
+import io.jmix.flowui.component.textfield.TypedTextField;
+import io.jmix.flowui.kit.component.button.JmixButton;
+import io.jmix.flowui.model.CollectionLoader;
 import io.jmix.flowui.model.InstanceContainer;
 import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Objects;
 
 
 @Route(value = "scoreboards", layout = MainView.class)
@@ -23,6 +29,10 @@ public class ScoreboardListView extends StandardListView<Scoreboard> {
     private DataManager dataManager;
     @ViewComponent
     private DataGrid<Scoreboard> scoreboardsDataGrid;
+    @ViewComponent
+    private TypedTextField<Object> searchField;
+    @ViewComponent
+    private CollectionLoader<Scoreboard> scoreboardsDl;
 
     @Subscribe(id = "scoreboardsDc", target = Target.DATA_CONTAINER)
     public void onScoreboardsDcItemPropertyChange(final InstanceContainer.ItemPropertyChangeEvent<Scoreboard> event) {
@@ -40,6 +50,18 @@ public class ScoreboardListView extends StandardListView<Scoreboard> {
                         ((Focusable) editorComponent).focus();
                     }
                 });
+    }
+
+    @Subscribe(id = "searchBtn", subject = "clickListener")
+    public void onSearchBtnClick(final ClickEvent<JmixButton> event) {
+       var searchText = searchField.getValue();
+
+        if (Objects.nonNull(searchText) && !searchText.isBlank()) {
+            scoreboardsDl.setParameter("searchText", "(?i).*" + searchText + ".*");
+        } else {
+            scoreboardsDl.removeParameter("searchText");
+        }
+        scoreboardsDl.load();
     }
 
 
